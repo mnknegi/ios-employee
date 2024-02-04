9615ed8ebda72231d6aafd3a34fe782f231c83af
imayanknegi29 <imayanknegi29@gmail.com>
2024-02-04 15:07:12 +0530

    EmployeeProfileViewController added programmatically.

---
+++ Employee/viewModels/EmployeeProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmployeeProfileViewModel.swift
+//  Employee
+//
+//  Created by Mayank Negi on 03/02/24.
+//
+
+import Foundation
---
+++ Employee/views/EmployeeProfileHeaderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmployeeProfileHeaderTableViewCell.swift
+//  Employee
+//
+//  Created by Mayank Negi on 03/02/24.
+//
+
+import UIKit
+
+class EmployeeProfileHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
